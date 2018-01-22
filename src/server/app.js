f3d971407909a2a50b5280615528265cbbbb00d7
Lunya <ludovic.sannicolas@gmail.com>
2018-01-22 15:22:39 +0100

    Avancement de l'interface web.

--- src/server/app.js
+++ src/server/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
-const createEngine = require('node-twig').createEngine;
+const bodyParser = require('body-parser');
+let createEngine = require('node-twig').createEngine;
 const hbase = require('hbase');
 
 process.env.SERVER_PORT = 4200;
@@ -14,20 +15,42 @@ app.engine('.twig', createEngine({
 }));
 app.set('views', './views');
 app.set('view engine', 'twig');
+app.use(bodyParser.json());
 
 app.get('/test', (req, res) => {
 	res.status(200).render('index', {
 		context: {
-			name: 'world'
+			projectName: 'PLE_DLM'
 		}
 	});
 });
 
 app.use(express.static(path.join(__dirname, './static')));
+app.use('/ajax.js', express.static(path.join(__dirname, './node_modules/client-ajax/index.js')));
 
 app.get('/', (req, res) => {
 	res.contentType('text/html');
 	res.status(200).send("Hello world!");
+});
+
+app.post('/image', (req, res) => {
+	console.log(req.body);
+	console.log(`lat: ${req.body.lat} lng: ${req.body.lng} img: ${req.body.img}`);
+	const imageSize = 256;
+	const imageTypeSize = 2;
+	//let imgBuffer = new ArrayBuffer(imageSize * imageSize * imageTypeSize);
+	/*let imgBuffer = new Buffer(imageSize * imageSize * imageTypeSize);
+	let imgArray = new Uint16Array(imgBuffer);*/
+	let imgArray = new Uint16Array(imageSize * imageSize);
+
+	for (let y = 0; y < imageSize; y++) {
+		for (let x = 0; x < imageSize; x++) {
+			imgArray[y * imageSize + x] = y * imageSize + x;
+		}
+	}
+
+	//res.contentType('application/octet-stream');
+	res.status(200).end(new Buffer(imgArray.buffer), 'binary');
 });
 
 const server = http.createServer(app);
--- src/server/static/map.js
+++ src/server/static/map.js
@@ -1,7 +1,52 @@
+const LATITUDE_MIN = -90.0;
+const LATITUDE_MAX = 90.0;
+const LONGITUDE_MIN = -180.0;
+const LONGITUDE_MAX = 180.0;
+
 let Point = function(x, y) {
 	this.x = x || 0;
 	this.y = y || 0;
 };
+
+let MapImage = function(data, size, position) {
+	// private functions
+	this.buildImage = function() {
+		let canvas = document.createElement('canvas');
+		let ctx = canvas.getContext('2d');
+		canvas.width = this.size.x;
+		canvas.height = this.size.y;
+		let id = ctx.createImageData(this.size.x, this.size.y);
+		let data = id.data;
+		for (let i = 0, size = this.data.length; i < size; i++) {
+			let I = i * 4;
+			data[I] = this.data[i] / 256;// / 8000;
+			data[I+1] = this.data[i] % 256;// / 8000;
+			data[I+2] = this.data[i] / 256;// / 8000;
+			data[I+3] = 255;
+		}
+		ctx.putImageData(id, 0, 0);36
+		ctx.font = '22px monospace';
+		ctx.fillStyle = '#ff0';
+		ctx.strokeStyle = '#f0f';
+		const text = `${this.position.x}E;${this.position.y}N`;
+		ctx.fillText(text, 20, 42);
+		ctx.strokeText(text, 20, 42);
+		this._image.src = canvas.toDataURL('image/png');
+	};
+
+	// public functions
+	this.getImage = function() {
+		this._lastAccess = Date.now();
+		return this._image;
+	};
+
+	// constructor
+	this.data = data; // Bitmap
+	this.size = size; // Point
+	this.position = position; // Point
+	this._image = new Image();
+	this._lastAccess = Date.now();
+}
 
 let Map = function(parent) {
 	// private functions
@@ -12,6 +57,14 @@ let Map = function(parent) {
 		this.size.y = this.parent.clientHeight;
 		this.canvas.setAttribute('width', this.size.x + 'px');
 		this.canvas.setAttribute('height', this.size.y + 'px');
+	};
+
+	this.update = function() {
+		for (let i = 0; i < this.images.length; i++) {
+			let image = this.images[i];
+			this.ctx.drawImage(image.getImage(), image.position.x, image.position.y);
+		}
+		// draw meridians
 	};
 
 	// public functions
@@ -29,13 +82,26 @@ let Map = function(parent) {
 	this.canvas.classList.add('map');
 	this.parent.appendChild(this.canvas);
 
+	this.images = [];
+
+	this.zoom = 1.0; // zoom = pixel * degree
 	this.size = new Point(this.parent.clientWidth, this.parent.clientHeight);
 	this.resizeCanvas();
 
 	// bindings
 	window.addEventListener('resize', function(e) {
 		this.resizeCanvas();
-		this.ctx.lineWidth = 5;
-		this.ctx.strokeRect(0, 0, this.size.x, this.size.y);
 	}.bind(this));
+	this.canvas.addEventListener('mousemove', function(e) {
+		this.ctx.beginPath();
+		this.ctx.arc(
+			e.clientX - e.target.offsetLeft,
+			e.clientY - e.target.offsetTop,
+			5, 0, 2*Math.PI);
+		this.ctx.stroke();
+	}.bind(this));
+	this.canvas.addEventListener('wheel', function(e) {
+		e.preventDefault();
+		console.log(e);
+	});
 };
